73f441eca705e364598e2db74f47df096260d466
Ammatech <tony.sykes@ammatech.com.au>
2016-06-16 15:54:43 +1000

    Initial

--- src/app/assure.component.ts
+++ src/app/assure.component.ts
@@ -1,11 +1,86 @@
-import { Component } from '@angular/core';
+import {Component, OnInit}         from '@angular/core';
+import { Router, RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+
+import {RfidMonitorFormComponent} from './rfid-monitor-form.component';
+import {RfidReaderFormComponent} from './rfid-reader-form.component';
+import {UserProfileFormComponent} from "./user-profile-form.component";
+
+import {AdministrationService} from "./administration.service";
+import {RfidMonitorService} from "./rfid-monitor.service";
+import {UserProfileService} from "./user-profile.service";
+import {UtilityService} from "./utility.service";
 
 @Component({
-  moduleId: module.id,
   selector: 'assure-app',
-  templateUrl: 'assure.component.html',
-  styleUrls: ['assure.component.css']
+  templateUrl: 'app/assure.component.html',
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS, AdministrationService, UtilityService]
 })
-export class AssureAppComponent {
-  title = 'assure works!';
+@RouteConfig([
+    {
+        path: '/rfidmonitor',
+        name: 'RfidMonitor',
+        component: RfidMonitorFormComponent
+    },
+    {
+        path: '/rfidreader',
+        name: 'RfidReader',
+        component: RfidReaderFormComponent
+    },
+    {
+        path: '/userprofile',
+        name: 'UserProfile',
+        component: UserProfileFormComponent
+    },
+])
+
+export class AssureAppComponent implements OnInit {
+
+    entitys:any = [];
+    entitySelect = false;
+    entityId:number;
+    title:string = 'Next Gen Assure';
+
+    userTypes:any = [];
+    userStatuses:any = [];
+
+    constructor(private _router: Router,
+    private _http: AdministrationService,
+    private _httpUtilityService: UtilityService) {
+
+    }
+
+    ngOnInit() {
+        console.log('AssureAppComponent - ngOnInit called');
+        this.onGetEntity();
+        //this.onGetUserType();
+    }
+
+
+    onGetEntity() {
+        this._http.getEntity()
+            .subscribe(
+                data => this.entitys = data,
+                error => console.log(error), //alert(error.toString()),
+                () => console.log('getEntity Finished')
+            )
+    }
+
+    //onGetUserType() {
+    //    this._httpUtilityService.getUserType()
+    //        .subscribe(
+    ///            data => {
+    //               this.userTypes   = data;
+    //                console.log('onGetUserType = ' + JSON.stringify(data) );
+    //            },
+    //            error => console.log(error), //alert(error.toString()),
+    //            () => console.log('onGetUserType Finished')
+    //        )
+    //}
+
+    onSelectEntity(entityId: number) {
+        console.log('onSelectEntity Called')
+        this.entitySelect = true;
+        this.entityId = entityId;
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { HTTP_PROVIDERS } from '@angular/http';
+import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import { enableProdMode } from '@angular/core';
 import { AssureAppComponent, environment } from './app/';
 
@@ -6,5 +8,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AssureAppComponent);
+bootstrap(AssureAppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS])
+  .catch(err => console.error(err));
+
 
